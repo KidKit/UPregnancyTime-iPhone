4a3c3ec73907938792f7bd5d071a479d8afd76aa
Harold Jiang <jzsues@gmail.com>
2012-04-07 21:28:39 +0800

    Initial Commit

---
+++ UPregnancyTime/UBTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UBTAppDelegate.h
+//  UPregnancyTime
+//
+//  Created by Zhimin Jiang on 12-4-7.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UBTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
